30b1c184101490d9426225f8cb6b28fff0a3ebff
MarryBye <viktorlukianov@outlook.com>
2023-03-02 10:22:37 +0200

    Update

--- Lab1/Lab1_Arithmetical.cpp
+++ Lab1/Lab1_Arithmetical.cpp
@@ -9,16 +9,18 @@ int main()
     cout << "The program is needed to calculate the value of an equation with numbers that the user enters." << endl;
     cout << "For the calculation, you need to enter 2 numbers x and y.." << endl;
 
-    cout << "10: z = (2cos(x - pi / 6) / 0.5 + sin^2y)* (1 + x^2 / 3 - y^2 / 5))" << endl;
+    cout << "10: z = (2cos(x - pi / 6) / 0.5 + sin^2y)* (1 + x^2 / 3 - y^2 / 5))\n" << endl;
 
     float x = 0;
     float y = 0;
 
-    cout << "\nEnter X: ";
+    cout << "Enter X: ";
     cin >> x;
+    cout << endl;
 
-    cout << "\nEnter Y: ";
+    cout << "Enter Y: ";
     cin >> y;
+    cout << endl;
 
     float result_1 = 2 * cos(x - M_PI / 6);
     float result_2 = 0.5 + pow(sin(y), 2);
@@ -27,7 +29,7 @@ int main()
 
     float result = result_1 / result_2 * (1 + result_3 / result_4);
 
-    cout << "\nResult is: " << result << endl;
+    cout << "Result is: " << result << endl;
 
     system("pause");
     return 0;
--- Lab1/Lab1_Trapezoid.cpp
+++ Lab1/Lab1_Trapezoid.cpp
@@ -6,24 +6,27 @@ int main()
 {
 
     cout << "The program is needed to calculate the area of a trapezoid." << endl;
-    cout << "For the calculation, you need the height and base of the figure." << endl;
+    cout << "For the calculation, you need the height and base of the figure.\n" << endl;
 
     float height = 0;
     float base_first = 0;
     float base_second = 0;
 
-    cout << "\nEnter height: ";
+    cout << "Enter height: ";
     cin >> height;
+    cout << endl;
 
-    cout << "\nEnter base 1: ";
+    cout << "Enter base 1: ";
     cin >> base_first;
+    cout << endl;
 
-    cout << "\nEnter base 2: ";
+    cout << "Enter base 2: ";
     cin >> base_second;
+    cout << endl;
 
     float area = (height * (base_first + base_second)) / 2;
 
-    cout << "\n3The area of a trapezoid with height = " << height << ", base 1 = " << base_first << ", base 2 = "
+    cout << "The area of a trapezoid with height = " << height << ", base 1 = " << base_first << ", base 2 = "
     << base_second << " is " << area << endl;
 
     system("pause");
