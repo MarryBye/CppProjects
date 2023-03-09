872d660baccc34d5c0a0355ba55e56a35a37f850
MarryBye <viktorlukianov@outlook.com>
2023-03-09 21:04:25 +0200

    Update

--- Lab4/Lab4_multiprogram.cpp
+++ Lab4/Lab4_multiprogram.cpp
@@ -16,24 +16,23 @@ int main() {
             cout << "Программа для решения квадратного уравнения Ax 2 + Bx +\n"
                     "   C = 0. A , B и C вводятся с клавиатуры." << endl;
 
-            int a, b, c, d;
-            float sqrd, x1, x2;
+            float A, B, C, d, sqrd, x1, x2;
 
             cout << "Enter A: ";
-            cin >> a;
+            cin >> A;
             cout << endl;
 
             cout << "Enter B: ";
-            cin >> b;
+            cin >> B;
             cout << endl;
 
             cout << "Enter C: ";
-            cin >> c;
+            cin >> C;
             cout << endl;
 
-            cout << a << "x^2 + " << b << "x + " << c << " = 0" << endl;
+            cout << A << "x^2 + " << B << "x + " << C << " = 0" << endl;
 
-            d = (pow(b, 2)) - (4 * a * c);
+            d = (pow(B, 2)) - (4 * A * C);
             cout << "Discriminant: " << d << endl;
 
             sqrd = sqrt(d);
@@ -41,11 +40,11 @@ int main() {
             if (d < 0) {
                 cout << "No roots!" << endl;
             } else if (d == 0) {
-                x1 = (-1 * b + sqrd) / (2 * a);
+                x1 = (-1 * B + sqrd) / (2 * A);
                 cout << "x: " << x1 << endl;
             } else {
-                x1 = (-1 * b + sqrd) / (2 * a);
-                x2 = (-1 * b - sqrd) / (2 * a);
+                x1 = (-1 * B + sqrd) / (2 * A);
+                x2 = (-1 * B - sqrd) / (2 * A);
 
                 cout << "x1: " << x1 << endl;
                 cout << "x2: " << x2 << endl;
@@ -55,8 +54,7 @@ int main() {
         case 2:
             cout << "Решить уравнение a|x|=b." << endl;
 
-            int a, b;
-            float x;
+            float a, b, x;
 
             cout << "Enter a: ";
             cin >> a;
@@ -66,7 +64,20 @@ int main() {
             cin >> b;
             cout << endl;
 
+            x = b / a;
 
+            if (x < 0) {
+                cout << "No roots!" << endl;
+            } else if (a == 0 && b != 0) {
+                cout << "No roots!" << endl;
+            } else if (a == 0 && b == 0) {
+                cout << "x E R" << endl;
+            } else {
+                cout << a << "|x| = " << b << endl;
+                cout << "|x| = " << b << " / " << a << endl;
+                cout << "|x| = " << x << endl;
+                cout << "x1 = " << x << " | x2 = " << x * -1 << endl;
+            }
 
             break;
         case 3:
