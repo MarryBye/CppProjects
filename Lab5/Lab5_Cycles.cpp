50cbcdd2ec3a61628480fc4146809149e64f4214
MarryBye <viktorlukianov@outlook.com>
2023-03-10 17:01:53 +0200

    Update

---
+++ Lab5/Lab5_Cycles.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Привет!" << endl;
+
+    system("pause");
+    return 0;
+
+}
